461233ecb3d46f335cbdc4c1716c5e54aef134a1
szymon-kowal <00szymon.kowal@gmail.com>
2023-07-19 13:18:33 +0200

    Add user validation for password and confirm password inputs.

--- javasc.js
+++ javasc.js
@@ -17,3 +17,46 @@ passwordError = password.nextElementSibling;
 
 confirmPassword = document.getElementById(names[5]);
 confirmPasswordError = confirmPassword.nextElementSibling;
+
+password.addEventListener('input',validatePassword);
+confirmPassword.addEventListener('input',validatePassword);
+
+
+const regexPassword = /^[a-zA-Z0-9_\-]+$/;
+
+function validatePassword() {
+    const passwordValue = password.value;
+    const confirmPasswordValue = confirmPassword.value;
+
+    const thisField = this;
+
+
+    if (thisField === password) {
+        if (!regexPassword.test(passwordValue)) {
+            passwordError.style.opacity = 1;
+            passwordError.textContent = 'Invalid input.';
+        } else if (passwordValue !== confirmPasswordValue) {
+            confirmPasswordError.style.opacity = 1;
+            confirmPasswordError.textContent = 'Passwords do not match.';
+        } else {
+            passwordError.style.opacity = 0;
+            passwordError.textContent = 'a';
+            confirmPasswordError.style.opacity = 0;
+            validationMessage.textContent = 'a';
+        }
+    } else if (thisField === confirmPassword) {
+        if (passwordValue !== confirmPasswordValue) {
+            confirmPasswordError.style.opacity = 1;
+            confirmPasswordError.textContent = 'Passwords do not match.';
+          } else if (!regexPassword.test(confirmPasswordValue)) {
+            passwordError.style.opacity = 1;
+            passwordError.textContent = 'Invalid input.';
+          } else {
+            confirmPasswordError.style.opacity = 0;
+            validationMessage.textContent = 'a';
+            passwordError.style.opacity = 0;
+            passwordError.textContent = 'a';
+          }
+    }
+    
+}
